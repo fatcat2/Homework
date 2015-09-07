a41321d7d835ce6b719965d5af06fe90dd48e95d
Ryan Chen <ryanjchen@yahoo.com>
2015-09-06 17:53:49 -0700

    Added a method to calculate

--- H5 - MPG/src/Engine.java
+++ H5 - MPG/src/Engine.java
@@ -10,7 +10,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 public class Engine {
-	@SuppressWarnings({ "static-access", "unused" })
+	@SuppressWarnings({ "static-access" })
 	public static void main(String[] args){
 		final String[] cars = new String[]{"Corvette", "F-150", "Motorcycle"};
 		final Double[] gasTank = new Double[]{19.1, 36.0, 8.0};
@@ -22,7 +22,8 @@ public class Engine {
 		final JButton b2 = new JButton("Fill UP DA METER");
 		final JButton b3 = new JButton("Choose car");
 		final JButton b4 = new JButton("DRIVE");
-		double gasTankSize;
+		String mileString;
+		double gasTankSize = 1;
 		b1.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -62,6 +63,10 @@ public class Engine {
 		}else if(carChoose.getText().equals("Motorcycle")){
 			gasTankSize = gasTank[2];
 		}
+		int miles = Integer.parseInt(enterMiles.getText());
+		
+		output.setText("You traveled: " + miles + "\nYour Tank Size is: " + gasTankSize + "\nAnd your mileage is: " + calcMPG(gasTankSize, miles) + "mpg!");
+		
 		JPanel p2 = new JPanel();
 		JPanel p3 = new JPanel();
 		JFrame frame = new JFrame("MPG Calculator 2000");
@@ -83,5 +88,9 @@ public class Engine {
 		frame.add(p2, BorderLayout.EAST);
 		frame.add(p3, BorderLayout.CENTER);
 		frame.add(output, BorderLayout.WEST);
+	}
+	public static double calcMPG(double tankSize, int mileage){
+		double mpg = mileage / tankSize;
+		return mpg;
 	}
 }
