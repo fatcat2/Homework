3a338e7d3848c06fb16577ae5ace131bb81bb697
Ryan Chen <ryanjchen@yahoo.com>
2015-09-07 18:20:49 -0700

    Finished and comment coded!

--- H5 - MPG/src/Car.java
+++ H5 - MPG/src/Car.java
@@ -1,26 +1,41 @@
-
+/*
+ * Author: fatcat2
+ * Date: September 7th, 2015
+ * Description: The car class, which contains methods and variables pertaining to its mileage and 
+ * gas tank.
+ */
 public class Car {
+	
+	//Creating and initializing main variables.
 	double odometer = 0;
-	int full = 20;
-	int empty = 0;
-	double gasUsed;
+	double gasUsed = 0;
+	
+	//Empty constructor to create the object.
 	public Car(){
 		
 	}
+	
+	//Method returns String, calculates and returns the answer and steps.
 	public String stopAtTheGasStation(double m, double g){
 		odometer = m;
 		gasUsed = g;
 		return "<html>You travelled: " + m + " miles.<br>" + "You used: " + g + " gallons.<br>" + "And your mileage is: "+ calcMPG() + " mpg!</html>";
 	}
+	
+	//Calculates the MPG
 	public double calcMPG(){
 		return odometer/gasUsed;
 	}
+	
+	//Obligatory mutator methods.
 	public void setMiles(double x){
 		odometer = x;
 	}
 	public void setGas(double x){
 		gasUsed = x;
 	}
+	
+	//Obligatory accessor methods.
 	public double getMiles(){
 		return odometer;
 	}
--- H5 - MPG/src/Engine.java
+++ H5 - MPG/src/Engine.java
@@ -15,41 +15,49 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 public class Engine {
-	static JFrame frame;
-	static JPanel p;
-	static JPanel p2;
-	
 	public static void main(String[] args){
-		p = new JPanel();
-		p2 = new JPanel();
-		frame = new JFrame("MPG Calculator 2000");
-		init(frame, p, p2);
+		
+		//Declaring panels and frame.
+		JFrame frame = new JFrame("MPG Calculator 2000");
+		JPanel p = new JPanel();
+		JPanel p2 = new JPanel();
+		
+		//Initializing frame
+		frame.setSize(800, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Declaring and initializing final variables, will be used in the ActionListener.
 		final JTextField enterMiles = new JTextField(15);
-		final JLabel output = new JLabel("Your mileage will be shown here.");
 		final JTextField enterGasUsed = new JTextField(5);
+		final JLabel output = new JLabel("Your mileage will be shown here.");
 		final Car c = new Car();
+		
+		//Declaring and init JLabels and buttons.
 		JLabel instructions = new JLabel("<html><h1>Welcome to the MPG Calculator 3000!</h1></html>");
 		JLabel i2 = new JLabel("<html>Please enter the amount of miles in the top box, and the number of gallons used in the lower box.</html>");
-		init(frame, p, p2);
 		JButton b4 = new JButton("DRIVE");
 		b4.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				//Using the mutator methods of the Car class to set the values to be used the calculate.
 				c.setMiles(Double.parseDouble(enterMiles.getText()));
 				c.setGas(Double.parseDouble(enterGasUsed.getText()));
 				writeOut(output, c);
 			}
 		});
+		//Adding the components to the frames and panels.
 		addPanel(new JComponent[]{instructions, i2, output}, p);
 		addPanel(new JComponent[]{enterMiles, enterGasUsed, b4}, p2);
 		endFrame(new JComponent[]{p, p2}, frame);
 	}
+	//This method is adds an array of components to the target JPanel.
 	public static void addPanel(JComponent[] cList, JPanel target) {
 		target.setLayout(new GridLayout(3, 1));
 		for(int i = 0; i < cList.length; i++){
 			target.add(cList[i]);
 		}
 	}
+	//Same thing, but for the frame, also includes setVisible().
 	public static void endFrame(JComponent[] cList, JFrame frame){
 		frame.setLayout(new GridLayout(1, 2));
 		for(int i = 0; i < cList.length; i++){
@@ -57,10 +65,7 @@ public class Engine {
 		}
 		frame.setVisible(true);
 	}
-	public static void init(JFrame f, JPanel p, JPanel p2){
-		f.setSize(800, 500);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+	//This method sets the text of the JLabel to the result using the method stopAtTheGasStation().
 	public static void writeOut(JLabel output, Car c){
 		try{
 			output.setText(c.stopAtTheGasStation(c.getMiles(), c.getGas()));
