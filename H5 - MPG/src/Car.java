5b144defeb639c2ecafc987117a1a365ae559f5d
Ryan Chen <ryanjchen@yahoo.com>
2015-09-07 18:09:43 -0700

    Changed everything to match instructions.

--- H5 - MPG/src/Car.java
+++ H5 - MPG/src/Car.java
@@ -1,17 +1,30 @@
 
 public class Car {
-	int gas;
-	int miles;
-	int type;
+	double odometer = 0;
 	int full = 20;
-	public Car(int g, int m){
-		gas = g;
-		miles = m;
+	int empty = 0;
+	double gasUsed;
+	public Car(){
+		
 	}
-	public void fillTank(int x){
-		gas = full;
+	public String stopAtTheGasStation(double m, double g){
+		odometer = m;
+		gasUsed = g;
+		return "<html>You travelled: " + m + " miles.<br>" + "You used: " + g + " gallons.<br>" + "And your mileage is: "+ calcMPG() + " mpg!</html>";
 	}
-	public int getGas(){
-		return gas;
+	public double calcMPG(){
+		return odometer/gasUsed;
+	}
+	public void setMiles(double x){
+		odometer = x;
+	}
+	public void setGas(double x){
+		gasUsed = x;
+	}
+	public double getMiles(){
+		return odometer;
+	}
+	public double getGas(){
+		return gasUsed;
 	}
 }
--- H5 - MPG/src/Engine.java
+++ H5 - MPG/src/Engine.java
@@ -15,85 +15,55 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 public class Engine {
-	
-	static double gasTankSize = 1;
-	static String[] cars = new String[]{"Corvette", "F-150", "Motorcycle"};
-	static JLabel carChoose = new JLabel("Corvette");
-	static Double[] gasTank = new Double[]{19.1, 36.0, 8.0};
-	static JTextField enterMiles = new JTextField(15);
-	static JLabel output = new JLabel("<html>Welcome to the MPG Calculator 3000!<br>Please enter the amount of miles you would like to travel.<br>You are able to choose from one of the cars below.<br>Just click the choose car button to cycle through the choices.<br>Each car has a different sized gas tank.<br> It will affect the end result.</html>");
 	static JFrame frame;
+	static JPanel p;
 	static JPanel p2;
-	static JPanel p3;
-	static int carCount = 0;
 	
 	public static void main(String[] args){
-		init();
-		JButton b3 = new JButton("Choose car");
+		p = new JPanel();
+		p2 = new JPanel();
+		frame = new JFrame("MPG Calculator 2000");
+		init(frame, p, p2);
+		final JTextField enterMiles = new JTextField(15);
+		final JLabel output = new JLabel("Your mileage will be shown here.");
+		final JTextField enterGasUsed = new JTextField(5);
+		final Car c = new Car();
+		JLabel instructions = new JLabel("<html><h1>Welcome to the MPG Calculator 3000!</h1></html>");
+		JLabel i2 = new JLabel("<html>Please enter the amount of miles in the top box, and the number of gallons used in the lower box.</html>");
+		init(frame, p, p2);
 		JButton b4 = new JButton("DRIVE");
-		JPanel p2 = new JPanel();
-		JPanel p3 = new JPanel();
-		b3.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-                setGasAndCar();
-			}
-		});
 		b4.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				writeOut();
+				c.setMiles(Double.parseDouble(enterMiles.getText()));
+				c.setGas(Double.parseDouble(enterGasUsed.getText()));
+				writeOut(output, c);
 			}
 		});
-		JComponent[] cList1 = new JComponent[]{b3, carChoose}; //p2
-		JComponent[] cList2 = new JComponent[]{enterMiles, b4};
-		JComponent[] cList3 = new JComponent[]{output, enterMiles, p2, p3};
-		addFrame(cList1, p2);
-		addFrame(cList2, p3);
-		addFrame(cList3, frame);
+		addPanel(new JComponent[]{instructions, i2, output}, p);
+		addPanel(new JComponent[]{enterMiles, enterGasUsed, b4}, p2);
+		endFrame(new JComponent[]{p, p2}, frame);
 	}
-	
-	
-	public static void addFrame(JComponent[] cList, JFrame target) {
-		frame.setLayout(new GridLayout(3, 2));
-		for(int i = 0; i < cList.length; i++){
-			target.add(cList[i]);
-		}
-		frame.pack();
-	}
-
-
-	public static void addFrame(JComponent[] cList, JComponent target){
+	public static void addPanel(JComponent[] cList, JPanel target) {
+		target.setLayout(new GridLayout(3, 1));
 		for(int i = 0; i < cList.length; i++){
 			target.add(cList[i]);
 		}
 	}
-	public static double calcMPG(double tankSize, int mileage){
-		double mpg = mileage / tankSize;
-		return mpg;
-	}
-	public static void init(){
-		p2 = new JPanel();
-		p3 = new JPanel();
-		frame = new JFrame("MPG Calculator 2000");
-		frame.setSize(800, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public static void endFrame(JComponent[] cList, JFrame frame){
+		frame.setLayout(new GridLayout(1, 2));
+		for(int i = 0; i < cList.length; i++){
+			frame.add(cList[i]);
+		}
 		frame.setVisible(true);
 	}
-	public static double getGasTankSize(){
-		return gasTankSize;
+	public static void init(JFrame f, JPanel p, JPanel p2){
+		f.setSize(800, 500);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	public static void setGasAndCar(){
-        carCount++;
-        if(carCount > 2){carCount = 0;}
-        carChoose.setText(cars[carCount]);
-        gasTankSize = gasTank[carCount];
-	}
-	public static void writeOut(){
-		int miles;
+	public static void writeOut(JLabel output, Car c){
 		try{
-		miles = Integer.parseInt(enterMiles.getText());
-		output.setText("<html>You traveled: " + miles + " miles.<br>" + "Your gas tank size is: " + getGasTankSize() + " gallons.<br>" + "And your mileage is: "+ calcMPG(gasTankSize, miles) + " mpg!</html>");
+			output.setText(c.stopAtTheGasStation(c.getMiles(), c.getGas()));
 		}catch(NumberFormatException e1){
 			output.setText("Please enter a valid number from 0 to inifinity!");
 		}
