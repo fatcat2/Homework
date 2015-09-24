428c564ea830c0a10767737f0aad21038fd6f462
Ryan Chen <ryanjchen@yahoo.com>
2015-09-24 08:48:01 -0700

    change stuff

--- New folder/src/Engine.java
+++ New folder/src/Engine.java
@@ -4,25 +4,26 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 public class Engine {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		String line = null;
 		File file = new File("goods.txt");
 		File output = new File("output.txt");
+//		ArrayList<String> list = new ArrayList<String>();
 		try{
 			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(output)));
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String s = br.readLine();
-			String[] s1 = s.split(",");
-			for(int i = 1; i <= s1.length; i++){
-				s1[i - 1] = s1[i - 1].trim();
-				if(i % 2 == 0){
-					s1[i - 1] = s1[i-1] + "\n";
-				}
-				pw.printf("%-10s", s1[i - 1]);
+			String[] arr = s.split("[\"\"]");
+//			list.add(s.substring(0, s.indexOf(",")));
+//			list.add(s.substring(s.indexOf(",") + 1));
+//			pw.printf("%-10s", list.get(0));
+			for(int i = 0; i < arr.length; i += 2){
+				System.out.println(arr[i].trim());
+				System.out.println(arr[i + 1].trim());
 			}
-			pw.printf("%-10s", "-------------------");
 			pw.close();
 		}catch(Exception e){
 			e.printStackTrace();
