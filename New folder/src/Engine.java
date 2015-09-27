56f93177c57da6dc6ee20103d0d1d24d66fe7de3
Ryan Chen <ryanjchen@yahoo.com>
2015-09-27 10:21:40 -0700

    Changed stuff to arrayList

--- New folder/src/Engine.java
+++ New folder/src/Engine.java
@@ -5,16 +5,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Engine {
 	@SuppressWarnings("resource")
+	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args){
 		try{
 			File file = new File("goods.txt");
 			File output = new File("output.txt");
 			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(output)));
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			String[] itemList = toArray(br);
+			ArrayList<String> itemList = toArray(br);
 			userInterface(pw, itemList);
 			pw.close();
 			br.close();
@@ -22,7 +24,7 @@ public class Engine {
 			e.printStackTrace();
 		}
 	}
-	public static String[] toArray(BufferedReader br){
+	public static ArrayList<String> toArray(BufferedReader br){
 		String total = "";
 		String s;
 		try {
@@ -32,30 +34,37 @@ public class Engine {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		String[] temp = total.split("[\"\"]");
-		return temp;
+		String[] temp1 = total.split("[\"\"]");
+		ArrayList<String> temp2 = new ArrayList<String>();
+		for(int i = 0; i < temp1.length; i++){
+			temp2.add(temp1[i]);
+		}
+		return temp2;
 	}
 	
-	public static void viewReceipt(PrintWriter pw, String[] arr){
-		for(int i = 1; i < arr.length - 1; i = i +  4){
-			pw.printf("%-10s", arr[i].trim());
-			pw.printf("%-10s", arr[i + 2].trim());
+	public static void viewReceipt(PrintWriter pw, ArrayList<String> arr){
+		System.out.println("---Receipt Start---");
+		for(int i = 1; i < arr.size() - 1; i = i +  4){
+			pw.printf("%-10s", arr.get(i).trim());
+			pw.printf("%-10s", arr.get(i + 2).trim());
 			pw.print("\n");
 			
-			System.out.printf("%-10s", arr[i].trim());
-			System.out.printf("%-10s", arr[i + 2].trim());
+			System.out.printf("%-10s", arr.get(i).trim());
+			System.out.printf("%-10s", arr.get(i + 2).trim());
 			System.out.print("\n");
 		}
-		System.out.println("---------------------");
+		System.out.println("----Receipt End----");
 	}
-	public static void userInterface(PrintWriter pw, String[] arr){
-		Scanner scan = new Scanner(System.in);
+	
+	public static void userInterface(PrintWriter pw, ArrayList<String> arr){
 		System.out.println("Welcome to the Advanced Grocery List!");
 		viewReceipt(pw,arr);
 		System.out.println("Would you like to add anything to your receipt? (Y/N)");
 		if(scan.next().equals("Y")){
-			System.out.println("nope");
+			viewReceipt(pw,arr);
 		}else{
+			System.out.println("Final Receipt:");
+			viewReceipt(pw,arr);
 			System.out.println("Have a nice day!");
 		}
 	}
