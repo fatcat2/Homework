f5ec6b8ee8758524c6ef2adca269664c2edd7bc0
Ryan Chen <ryanjchen@yahoo.com>
2015-09-27 09:56:08 -0700

    sync

--- New folder/src/Engine.java
+++ New folder/src/Engine.java
@@ -4,54 +4,43 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.util.Scanner;
 public class Engine {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
-		String line = null;
-		File file = new File("goods.txt");
-		File output = new File("output.txt");
-//		ArrayList<String> list = new ArrayList<String>();
 		try{
+			File file = new File("goods.txt");
+			File output = new File("output.txt");
 			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(output)));
-			BufferedReader br = new BufferedReader(new FileReader(file));	
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			Scanner scan = new Scanner(System.in);
 			String total = "";
 			String s;
-			
-//			total = total + s;
-			
-//			boolean keepGoing = true;
-//			while(keepGoing){
-//				String s = br.readLine();
-//				if(s == null){
-//					keepGoing = false;
-//					System.out.println("stopped");
-//					break;
-//				}else{
-//					total = total + s;
-//				}
-//			}
-			
 			while((s = br.readLine()) != null){
 				total = total + s;
 			}
-			System.out.println("I'm done scanning");
 			String[] arr = total.split("[\"\"]");
 			
-			System.out.println(arr.length);
-			
-			for(int i = 1; i < arr.length - 1; i = i +  4){
-				System.out.println(i);
-				pw.printf("%-10s", arr[i].trim());
-				pw.printf("%-10s", arr[i + 2].trim());
-				pw.print("\n");
+			System.out.println("Welcome to the Advanced Grocery List!");
+			viewReceipt(pw, total, arr);
+			System.out.println("Would you like to add anything to your receipt?");
+			if(scan.next().equals("Y")){
 			}
-			System.out.println("I'm done writing to the file.");
-			
 			pw.close();
 			br.close();
 		}catch(Exception e){
 			e.printStackTrace();
+		}
+	}
+	public static void viewReceipt(PrintWriter pw, String total, String[] arr){
+		for(int i = 1; i < arr.length - 1; i = i +  4){
+			pw.printf("%-10s", arr[i].trim());
+			pw.printf("%-10s", arr[i + 2].trim());
+			pw.print("\n");
+			
+			System.out.printf("%-10s", arr[i].trim());
+			System.out.printf("%-10s", arr[i + 2].trim());
+			System.out.print("\n");
 		}
 	}
 }
