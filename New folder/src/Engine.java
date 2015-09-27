50b04520c1f79e0446f6e0efc0161150e18f653f
Ryan Chen <ryanjchen@yahoo.com>
2015-09-27 10:08:44 -0700

    Methodizing sutff

--- New folder/src/Engine.java
+++ New folder/src/Engine.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 public class Engine {
@@ -13,26 +14,29 @@ public class Engine {
 			File output = new File("output.txt");
 			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(output)));
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			Scanner scan = new Scanner(System.in);
-			String total = "";
-			String s;
-			while((s = br.readLine()) != null){
-				total = total + s;
-			}
-			String[] arr = total.split("[\"\"]");
-			
-			System.out.println("Welcome to the Advanced Grocery List!");
-			viewReceipt(pw, total, arr);
-			System.out.println("Would you like to add anything to your receipt?");
-			if(scan.next().equals("Y")){
-			}
+			String[] itemList = toArray(br);
+			userInterface(pw, itemList);
 			pw.close();
 			br.close();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
-	public static void viewReceipt(PrintWriter pw, String total, String[] arr){
+	public static String[] toArray(BufferedReader br){
+		String total = "";
+		String s;
+		try {
+			while((s = br.readLine()) != null){
+				total = total + s;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String[] temp = total.split("[\"\"]");
+		return temp;
+	}
+	
+	public static void viewReceipt(PrintWriter pw, String[] arr){
 		for(int i = 1; i < arr.length - 1; i = i +  4){
 			pw.printf("%-10s", arr[i].trim());
 			pw.printf("%-10s", arr[i + 2].trim());
@@ -41,6 +45,18 @@ public class Engine {
 			System.out.printf("%-10s", arr[i].trim());
 			System.out.printf("%-10s", arr[i + 2].trim());
 			System.out.print("\n");
+		}
+		System.out.println("---------------------");
+	}
+	public static void userInterface(PrintWriter pw, String[] arr){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to the Advanced Grocery List!");
+		viewReceipt(pw,arr);
+		System.out.println("Would you like to add anything to your receipt? (Y/N)");
+		if(scan.next().equals("Y")){
+			System.out.println("nope");
+		}else{
+			System.out.println("Have a nice day!");
 		}
 	}
 }
