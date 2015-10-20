d340c4b3cf8f67d113c66a8257d1b588fb9e4e27
Ryan Chen <ryanjchen@yahoo.com>
2015-10-20 09:08:45 -0700

    Cleaning up the Tens
    Doing good so far, should do some comment coding so i understand what's
    going on.

--- H15-RomanNumerals/src/Engine.java
+++ H15-RomanNumerals/src/Engine.java
@@ -18,12 +18,45 @@ public class Engine {
 		if(n / 1000 > 0){
 			
 		}else if(n / 100 > 0){
-			
+			int i = n/100;
+			if(i >= 4){
+				int j = 5 - i;
+				if(j >= 0){
+					for(int x = 0; x < Math.abs(j); x++){
+						System.out.print("x");
+					}
+					System.out.print("c");
+				}else if(j > -4){
+					System.out.print("d");
+					for(int x = 0; x < Math.abs(j); x++){
+						System.out.print("x");
+					}
+				}else{
+					System.out.print("xc");
+				}
+			}
 		}else if(n / 10 > 0){
 			int i = n/10;
-			System.out.print(i);
-			
-			
+			if(i >= 4){
+				int j = 5 - i;
+				if(j >= 0){
+					for(int x = 0; x < Math.abs(j); x++){
+						System.out.print("x");
+					}
+					System.out.print("l");
+				}else if(j > -4){
+					System.out.print("l");
+					for(int x = 0; x < Math.abs(j); x++){
+						System.out.print("x");
+					}
+				}else{
+					System.out.print("xc");
+				}
+			}else if(i < 4 && i >=0){
+				for(int x = 0; x < i; x++){
+					System.out.print("x");
+				}
+			}
 		}else if(n / 5 > 0){
 			underTen(n);
 		}else{
