5ced9aa1d47084569e3f5db42b8a2a47635f4fd5
Ryan Chen <ryanjchen@yahoo.com>
2015-10-22 08:30:46 -0700

    Created H16-BacktoSchool

--- H15-RomanNumerals/src/Engine.java
+++ H15-RomanNumerals/src/Engine.java
@@ -6,20 +6,16 @@ public class Engine {
 		Scanner scan = new Scanner(System.in);
 		int n = 0;
 		String s = "";
-		boolean keepGoing = true;
-		while(keepGoing){
-			try{
-				n = scan.nextInt();
-				keepGoing = false;
-			}catch(NumberFormatException e){
-				System.out.println("try again");
-			}
-		}
+		System.out.println("Please enter a number of at most 4999:     ");
+		n = scan.nextInt();
+		scan.close();
 		System.out.println(romanNumerals(n, 0, s));
 		
 	}
 	
 	public static String romanNumerals(int n, int count, String s){
+		//Making the program go through everything, if the value is less than the value, it won't matter since
+		//it would just return as blank.
 		s += thousands(n/1000);
 		s += hundreds(n%1000/100);
 		s += tens(n%100/10);
@@ -28,6 +24,7 @@ public class Engine {
 	}
 	
 	public static String thousands(int n){
+		//Simpler algorithm than the others since it only goes to four.
 		String s = "";
 		for(int i = 0; i < n; i++){
 			s += "m";
@@ -36,16 +33,17 @@ public class Engine {
 	}
 	
 	public static String hundreds(int n){
+		//Returns a string
 		String s = "";
 		int i = n;
-		if(i >= 4){
+		if(i >= 4){ //if n is above or equals to 4
 			int j = 5 - i;
-			if(j >= 0){
-				for(int x = 0; x < Math.abs(j); x++){
+			if(j >= 0){ //for numbers under 5
+				for(int x = 0; x < Math.abs(j); x++){ //For loop to get the amount of c's
 					s = s + "c";
 				}
 				s =  s + "d";
-			}else if(j > -4){
+			}else if(j > -4){ //for numbers over 5
 				System.out.print("d");
 				for(int x = 0; x < Math.abs(j); x++){
 					s = s + "c";
