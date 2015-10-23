10423fb9c89d0e029310bfa79f1dd8d2d776c777
Ryan Chen <ryanjchen@yahoo.com>
2015-10-22 17:33:45 -0700

    Commit before gaming

--- P3-RecursiveArt/src/RectangleComponent.java
+++ P3-RecursiveArt/src/RectangleComponent.java
@@ -17,4 +17,13 @@ public class RectangleComponent extends JComponent{
 		Rectangle2D rect = new Rectangle2D.Double(50, 50, 50, 50);
 		g2.draw(rect);
 	}
+	public void recursiveColor(int x, int y, int w, int h, int colorfactor, Graphics2D g2){
+		if(w == 0 && h == 0){
+			
+		}else{
+			g2.setColor(new Color(((int)Math.sin(colorfactor))));
+			g2.fillRect(x, y, w, h);
+			recursiveColor(x - 1, y - 1, w - 1, h - 1, int )
+		}
+	}
 }
