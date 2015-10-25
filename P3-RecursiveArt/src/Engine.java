f647498691f702b31e5a0c650cfdbed1f70e7ced
fatcat2 <rjc.jumpi@gmail.com>
2015-10-25 12:23:39 -0700

    Commit from linux

--- P3-RecursiveArt/src/Engine.java
+++ P3-RecursiveArt/src/Engine.java
@@ -1,8 +1,11 @@
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 
 public class Engine {
 	public static void main(String[] args){
+//		Timer t = new Timer(30, new RectangleComponent());
+//		t.start();
 		JFrame frame = new JFrame();
 		frame.setSize(500,500);
 		frame.add(new RectangleComponent());
--- P3-RecursiveArt/src/RectangleComponent.java
+++ P3-RecursiveArt/src/RectangleComponent.java
@@ -1,18 +1,23 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 import javax.swing.JComponent;
 
 
-public class RectangleComponent extends JComponent{
+public class RectangleComponent extends JComponent implements ActionListener{
+	Random rand = new Random();
 	public void paintComponent(Graphics g){
 		Graphics2D g2 = (Graphics2D) g;
-//		drawRect(g2);
-		g2.setColor(Color.black);
-		g2.fillRect(50, 50, 50, 50);
-	}
+		drawRect(g2);
+		g2.setColor(new Color((rand.nextDouble(1)), (rand.nextInt(9)/10), (rand.nextInt(9)/10)));
+		
+//		recursiveColor(50, 50, 50, 50, 5, g2)
+;	}
 	public void drawRect(Graphics2D g2){
 		Rectangle2D rect = new Rectangle2D.Double(50, 50, 50, 50);
 		g2.draw(rect);
@@ -21,9 +26,11 @@ public class RectangleComponent extends JComponent{
 		if(w == 0 && h == 0){
 			
 		}else{
-			g2.setColor(new Color(((int)Math.sin(colorfactor))));
-			g2.fillRect(x, y, w, h);
-			recursiveColor(x - 1, y - 1, w - 1, h - 1, int )
+			g2.setColor(new Color((rand.nextInt(10)/10), (rand.nextInt(10)/10), (rand.nextInt(10)/10)));
+			g2.fill3DRect(x, y, w, h, true);
+			recursiveColor(x + 10, y + 10, w - 1, h - 1, colorfactor*2, g2);
 		}
+	}
+	public void actionPerformed(ActionEvent e) {
 	}
 }
