f643f19c070d39ddc259bc1272a9f062db734407
Ryan Chen <ryanjchen@yahoo.com>
2015-11-23 22:23:58 -0800

    finished h21

--- H21-IrregularGithub/src/Engine.java
+++ H21-IrregularGithub/src/Engine.java
@@ -3,6 +3,13 @@ public class Engine {
 
 	public static void main(String[] args) {
 		IrregularPolygon p = new IrregularPolygon();
+		p.add(-30, 5);
+		p.add(10, 5);
+		p.add(10, 19);
+		p.add(-30, 5);
+		p.draw();
+		System.out.println(p.area());
+		System.out.println(p.perimeter());
 	}
 
 }
--- H21-IrregularGithub/src/IrregularPolygon.java
+++ H21-IrregularGithub/src/IrregularPolygon.java
@@ -1,3 +1,7 @@
+/*
+ * by Ryan Chen
+ * i like pineapples
+ */
 import java.awt.geom.*;     // for Point2D.Double
 import java.util.ArrayList; // for ArrayList
 import gpdraw.*;            // for DrawingTool
@@ -18,7 +22,10 @@ public class IrregularPolygon{
 		if(poly.size() < 3){
 			System.out.println("This is not a polygon, please add " + (3 - poly.size()) + "more points.");
 		}else{
-			for(int i = 0; i < poly.size(); i++){
+			pencil.up();
+			pencil.move(poly.get(0).getX(), poly.get(0).getY());
+			pencil.down();
+			for(int i = 1; i < poly.size(); i++){
 				pencil.move(poly.get(i).getX(), poly.get(i).getY());
 			}
 		}
@@ -26,5 +33,47 @@ public class IrregularPolygon{
 	
 	public void add(double x, double y){
 		poly.add(new Point2D.Double(x, y));
+	}
+	
+	public double perimeter(){
+		double sum = 0;
+		for(int i = 0; i < poly.size(); i++){
+			if(i == poly.size()-1){
+				double a = Math.pow(poly.get(i).getX()-poly.get(0).getX(), 2);
+				double b = Math.pow(poly.get(i).getY()-poly.get(0).getY(), 2);
+				double x = Math.sqrt(a+b);
+				sum += x;
+			}else{
+				double a = Math.pow(poly.get(i).getX()-poly.get(i+1).getX(), 2);
+				double b = Math.pow(poly.get(i).getY()-poly.get(i+1).getY(), 2);
+				double x = Math.sqrt(a+b);
+				sum += x;
+			}
+		}
+		return sum;
+	}
+	
+	public double area(){
+		double sum = 0;
+		double sum2 = 0;
+		for(int i = 0; i < poly.size(); i++){
+			if(i == poly.size()-1){
+				double x = poly.get(poly.size()-1).getX() * poly.get(0).getY();
+				sum += x;
+			}else{
+				double x = poly.get(i).getX() * poly.get(i + 1).getY();
+				sum += x;
+			}
+		}
+		for(int i = 0; i < poly.size(); i++){
+			if(i == poly.size()-1){
+				double x = poly.get(poly.size()-1).getX() * poly.get(0).getY();
+				sum2 -= x;
+			}else{
+				double x = poly.get(i).getX() * poly.get(i + 1).getY();
+				sum2 -= x;
+			}
+		}
+		return (sum + sum2)/2;
 	}
 }
