58902b2a782ec996498755c6ed39b0ed27639439
Ryan Chen <ryanjchen@yahoo.com>
2015-11-29 22:29:49 -0800

    finished H23

--- H21-UseArrayLists/src/Engine.java
+++ H21-UseArrayLists/src/Engine.java
@@ -1,9 +1,10 @@
-import java.io.FileInputStream;
-
 public class Engine {
 
 	public static void main(String[] args) {
-		StatMan s = new StatMan("numbers.txt");
+		StatMan s = new StatMan("src/numbers.txt");
+		System.out.println("Average: " + s.getAvg());
+		System.out.println("Standard Deviation: " + s.stdDev());
+		System.out.println("Mode: " + s.getMode());
 	}
 
 }
--- H21-UseArrayLists/src/StatMan.java
+++ H21-UseArrayLists/src/StatMan.java
@@ -4,13 +4,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class StatMan {
-	ArrayList<Integer> numlist = new ArrayList<Integer>();
+	ArrayList<Integer> numList = null;;
 	BufferedReader br;
 	File f;
 	FileReader fr;
 	public StatMan(String file_path){
+		numList = new ArrayList<Integer>();
 		f = new File(file_path);
 		try {
 			fr = new FileReader(f);
@@ -22,11 +25,49 @@ public class StatMan {
 		String s;
 		try {
 			while((s = br.readLine()) != null){
-				
+				numList.add(Integer.parseInt(s));
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	public int getAvg(){
+		int x = 0;
+		for(int i = 0; i < numList.size(); i++){
+			x += numList.get(i);
+		}
+		return x;
+	}
+	
+	public double stdDev(){
+		int x = getAvg();
+		int y = 0;
+		for(int i = 0; i < numList.size(); i++){
+			y += x - numList.get(i);
+		}
+		return Math.sqrt(y/(numList.size()-1));
+	}
+	
+	public int getMode(){
+		Integer streak = 0;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		ArrayList<Integer> streakList = new ArrayList<Integer>();
+		Collections.sort(numList);
+		for(int i = 0; i < numList.size(); i++){
+			if(numList.get(i) == numList.get(i + 1)){
+				streak++;
+			}else{
+				map.put(numList.get(i), streak);
+				streakList.add(streak);
+				streak = 0;
+			}
+		}
+		Collections.sort(streakList);
+		return map.get(streakList.get(streakList.size()-1));
+		
+	}
+	
 }
+
