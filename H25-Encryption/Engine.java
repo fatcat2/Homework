77da41e31a7711b728e2ccd0005cf1474749333a
Ryan Chen <ryanjchen@yahoo.com>
2015-12-04 09:09:37 -0800

    Update Engine.java

--- H25-Encryption/Engine.java
+++ H25-Encryption/Engine.java
@@ -2,5 +2,8 @@ import javax.swing.JOptionPane;
 public class Engine{
     public static void main(String[] args){
         String s = JOptionPane.showInputDialog("Insert message");
+        int y = Integer.parseInt(JOptionPane.showInputDialog("Insert public key"));
+        int z = Integer.parseInt(JOptionPane.showInputDialog("Insert private key"));
+        CipherGen c = new CipherGen(s, y, z);
     }
 }
