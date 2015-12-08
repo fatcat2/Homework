c7717c39ca01e31d29ef0165bacbda68ac934d26
Ryan Chen <ryanjchen@yahoo.com>
2015-12-08 06:08:13 -0800

    Update DecryptEngine.java

--- H25-Encryption/DecryptEngine.java
+++ H25-Encryption/DecryptEngine.java
@@ -8,16 +8,17 @@ public class DecryptEngine{
         decrypt(a);
     }
     private static void decrypt(String s){
+         System.out.println("");
          String list[] = s.split("\\s");
          pub_key = Integer.parseInt(list[0]);
          for(int i = 1; i < list.length; i++){
              int x = Integer.parseInt(list[i]);
-             System.out.println(x);
+             //System.out.println(x);
              int their_key = pub_key / priv_key;
-             System.out.println(their_key);
+             //System.out.println(their_key);
              int z = x / their_key;
-             int a = z;
-             System.out.println(a);
+             char a = (char) z;
+             System.out.print(a);
          }
     }
 }
