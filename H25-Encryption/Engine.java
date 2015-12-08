19f81d602d5b92bd3b3a6930174b433ec754490d
Ryan Chen <ryanjchen@yahoo.com>
2015-12-08 09:09:59 -0800

    Update Engine.java

--- H25-Encryption/Engine.java
+++ H25-Encryption/Engine.java
@@ -1,9 +1,21 @@
 import javax.swing.JOptionPane;
+import java.io.*;
 public class Engine{
     public static void main(String[] args){
-        String s = JOptionPane.showInputDialog("Insert message");
+        String s = JOptionPane.showInputDialog("Insert file path");
+        String a = "";
+        String b = "";
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(s))));
+            while((a = br.readLine()) != null){
+                b += a + " ";
+            }
+        }catch(Exception e){
+            System.out.println("You gone fucked up!");
+        }
+        System.out.println("IT FUCKING RUNS");
         int y = Integer.parseInt(JOptionPane.showInputDialog("Insert public key"));
         int z = Integer.parseInt(JOptionPane.showInputDialog("Insert private key"));
-        CipherGen c = new CipherGen(s, y, z);
+        CipherGen c = new CipherGen(b, y, z);
     }
 }
